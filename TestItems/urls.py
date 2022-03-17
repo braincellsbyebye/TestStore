96d74f1500c972f010d3712d9cdb5a119102886a
JC <ezjcd101@gmail.com>
2022-03-17 02:40:38 -0700

    Created aboutus.html and layout.html

--- TestItems/urls.py
+++ TestItems/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main),
+    path('aboutus/', views.aboutus),
 ]
--- TestItems/views.py
+++ TestItems/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'index.html')
+
+def aboutus(request):
+    return render(request, 'aboutus.html')
